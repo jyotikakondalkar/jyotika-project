9f10e3f32578924942a4d27a6a0b4d945fb701b2
jyotikakondalkar <jyotica.kondalkar@gmail.com>
2016-09-09 16:15:04 +0530

    from office system. Map Module Implemented.

--- BestBuy1826/modules/js/animations.js
+++ BestBuy1826/modules/js/animations.js
@@ -151,3 +151,31 @@ function onClickSearchCancel(){
 	frmHome.flxFilterContainer.width = "2%";
 	frmHome.flxFilterContainer.height = "2%";
 }
+
+function moveFlexDownUp(eventobject) {
+return AS_Button_60d459e79c6b4c8885e37668e1721d2f(eventobject);
+}
+function AS_Button_60d459e79c6b4c8885e37668e1721d2f(eventobject) {
+function MOVE_ACTION____dddb79cf20ef4b139f73836968233b44_Callback(){
+
+}
+frmProductDetails["flxReviews"].animate(
+kony.ui.createAnimation({"100":{"top":"66%","stepConfig":{"timingFunction":kony.anim.EASE}}}),
+{"delay":0,"iterationCount":1,"fillMode":kony.anim.FILL_MODE_FORWARDS,"duration":0.3},
+ {"animationEnd" : MOVE_ACTION____dddb79cf20ef4b139f73836968233b44_Callback});
+
+}
+
+function moveFlexDownUp2(eventobject) {
+return AS_Button_60d459e79c6b4c8885e37668e1721d2f2(eventobject);
+}
+function AS_Button_60d459e79c6b4c8885e37668e1721d2f2(eventobject) {
+function MOVE_ACTION____dddb79cf20ef4b139f73836968233b44_Callback2(){
+
+}
+frmProductDetails["flxReviews"].animate(
+kony.ui.createAnimation({"100":{"top":"66%","stepConfig":{"timingFunction":kony.anim.EASE}}}),
+{"delay":0,"iterationCount":1,"fillMode":kony.anim.FILL_MODE_FORWARDS,"duration":0.3},
+ {"animationEnd" : MOVE_ACTION____dddb79cf20ef4b139f73836968233b44_Callback2});
+
+}
